cf7b4e1c350518d1e1b0b97b97c3ae49ba9e06e3
Kafaa Tools Developer <developer@kafaatools.com>
2025-07-22 23:40:45 +0300

    d

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-
+import { Analytics } from "@vercel/analytics/next"
 interface LayoutProps {
   children: ReactNode;
   className?: string;
@@ -8,6 +8,7 @@ interface LayoutProps {
 const Layout = ({ children, className = '' }: LayoutProps) => {
   return (
     <div className={`min-h-screen bg-gradient-background ${className}`} dir="rtl">
+      <Analytics />
       <div className="container mx-auto px-4 ">
         {children}
       </div>
